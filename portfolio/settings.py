8f8b018103fea4a2e8ce4db9bfc25097d5bfc354
nehal2210 <gatshabz221097@gmail.com>
2019-12-26 03:48:04 +0500

    add static folder

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES=[
+STATICFILES_DIRS=[
  os.path.join(BASE_DIR, 'portfolio/static/')   
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
