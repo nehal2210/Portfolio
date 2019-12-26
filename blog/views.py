8455c45659309018ccf3555c9a4ef4c662f19926
nehal2210 <gatshabz221097@gmail.com>
2019-12-26 17:17:54 +0500

    first version of portfolio web app completed

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from .models import Blog
 # Create your views here.
 def allblog(request):
-    return(render(request,'blog/allblog.html'))
+    blogs=Blog.objects.all()
+    return(render(request,'blog/allblog.html',{'blogs':blogs}))
+
+
+def detail(request,blog_id):
+    
+    detailblog=get_object_or_404(Blog,pk=blog_id)
+    
+    return(render(request,'blog/detail.html',{'detailblog':detailblog}))
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,13 +18,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
-import blog.views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   path('blog/', include('blog.urls')),
+
     path('', jobs.views.home,name='home'),
-    path('blog/', blog.views.allblog,name='blog'),
    
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
